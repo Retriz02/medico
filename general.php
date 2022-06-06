bd168b4b3cd2b88c7c5d7c9355d7e7502d20ac6b
Rebeca <beatrizsantacruz2@gmail.com>
2022-06-06 09:31:58 -0300

    ver1

---
+++ general.php
@@ -0,0 +1,9 @@
+<?php
+		$servidor='localhost';
+		$usuario='root';
+		$password='';
+		$base_de_datos1='general';
+		
+		$conexionGeneral= new MySQLI($servidor, $usuario, $password, $base_de_datos1);
+
+?>
---
+++ medicos.php
@@ -0,0 +1,9 @@
+<?php
+			$servidor='localhost';
+			$usuario='root';
+			$password='';
+			$base_de_datos2='medicos';
+			
+			$conexion= new MySQLI($servidor, $usuario, $password, $base_de_datos2);
+			
+?>	
